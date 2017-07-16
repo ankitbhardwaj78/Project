7f716c97c5e4801092bcd371c3c18eabedb4ef21
sanchittanwar7 <tanwarsanchit9762@gmail.com>
2017-07-16 16:09:55 +0530

    kuch bhi

--- app/controllers/material_controller.rb
+++ app/controllers/material_controller.rb
@@ -15,3 +15,4 @@ class MaterialController < ApplicationController
 
 
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :materials do
+  collection do
+    get :download
+  end
+end
+
 
   resources :materials
   get '/ad/index'
